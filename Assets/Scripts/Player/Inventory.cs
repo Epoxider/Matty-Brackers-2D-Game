fa93ebcb4c60fb1dd84a356aec787ffea14020f2
joseph grose <j_grose@live.com>
2021-06-20 11:18:38 -0400

    changed ui around, plus added title maps

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    // Start is called before the first frame update
+
+    private List<Item> itemList;
+
+    public Inventory() 
+    {
+        itemList = new List<Item>();
+    }
+    
+
+
+}
